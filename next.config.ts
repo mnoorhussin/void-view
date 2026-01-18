438585cbc9a463c07364bf19a3d5f884018a435e
Mustafa <mu88ali@gmail.com>
2026-01-18 19:20:09 +0100

    add  unoptimized: true, to vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images-assets.nasa.gov" },
       { protocol: "https", hostname: "images-api.nasa.gov" },
